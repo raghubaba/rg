878f531b11c4722ed0993934450822250ca20483
Raghu <raghavendrar420@gmail.com>
2023-05-11 13:18:21 +0530

    Hello wolrd hiiiii

---
+++ Demo.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Demo{
+public static void main(String...args)
+{
+	String s1="rahul";
+	String s2="rahul";
+	if(s1==s2){
+		System.out.println("Reference are equal");
+	}else{
+		System.out.println("Not equal to");
+	}
+}
+}
---
+++ first.js
@@ -0,0 +1,8 @@
+var i;
+var emp=["hhh","jjj","kkk","fff"]
+//console.log(emp)
+//   for(i=0;i<emp.length;i++){
+//      console.log(emp[i]);
+//   }
+emp.pop(emp[1])
+console.log(emp)
